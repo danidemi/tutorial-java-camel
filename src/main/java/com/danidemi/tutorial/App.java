51ca7cd5761587f1cd9c34b7506cf43f8ee3c8d1
danidemi <demichelis@danidemi.com>
2014-12-26 11:58:57 +0100

    App with basic route

--- src/main/java/com/danidemi/tutorial/App.java
+++ src/main/java/com/danidemi/tutorial/App.java
@@ -1,8 +1,16 @@
 package com.danidemi.tutorial;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+
 import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.direct.DirectComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class App {
@@ -20,20 +28,26 @@ public class App {
 
 		@Override
 		public void configure() throws Exception {
-			from("direct:a").to("direct:b");			
+					
+			from("file://source").to("file://destination");			
 		}
 		
 	}
 
 	private static void dodo() throws Exception {
+		
 		// CamelContext, a container for Components, Routes etc...
 		CamelContext context = new DefaultCamelContext();
 				
+		context.addRoutes( new MyAppRouteBuilder() );
+				
 		context.start();
+		
+		ProducerTemplate template = context.createProducerTemplate();	
+		template.sendBody("file://destination", "A message");
+		
 		context.stop();
 		
-//		RoutesBuilder builder = RouteBuilder.
-//		context.addRoutes( new MyAppRouteBuilder());
 	}
 	
 }
